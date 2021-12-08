1cf7cf3ebff7cabec5007296ec0fe8642fc70830
Ada Joule <ada.fulmina@gmail.com>
2021-12-09 03:44:19 +0700

    Data structure cleanup

---
+++ demystify/mtg_types.py
@@ -0,0 +1,71 @@
+from typing import Literal, NamedTuple, Union
+
+Color = Literal["white", "blue", "black", "red", "green"]
+
+class ManaSymbol(NamedTuple):
+    symbol: str
+    hybrid_symbol: Union[str, None] = None
+
+    @classmethod
+    def from_str(cls, symbol_str: str):
+        symbol_list = symbol_str.strip('{}').split('/')
+        l = len(symbol_list)
+        if l == 1:
+            return cls(symbol_list[0])
+        elif l == 2:
+            return cls(symbol_list[0], hybrid_symbol=symbol_list[1])
+        else:
+            raise ValueError("Incorrect number of mana symbol parts")
+
+    def __repr__(self):
+        if self.hybrid_symbol is None:
+            return '{%s}' % self.symbol
+        else:
+            return '{%s/%s}' % (self.symbol, self.hybrid_symbol)
+
+Mana = list[ManaSymbol]
+
+
+class Typeline(NamedTuple):
+    cardTypes: list[str]
+    supertypes: list[str] = []
+    subtypes: list[str] = []
+
+    def __repr__(self):
+        sup = (' '.join(self.supertypes) + ' ') if len(self.supertypes) > 0 else ''
+        typ = ' '.join(self.cardTypes)
+        sub = (' -- ' + ' '.join(self.subtypes)) if len(self.subtypes) > 0 else ''
+        return '%s%s%s' % (sup, typ, sub)
+
+class KeywordAbility(NamedTuple):
+    keyword: str
+    arg_int: Union[str, None] = None
+    arg_cost: Union[str, None] = None
+    arg_quality: Union[str, None] = None
+    arg_from: Union[list[str], None] = None
+
+class Subset(NamedTuple):
+    noun: str
+    adj: list[str]
+    descriptor: list[str]
+
+class Sentence(NamedTuple):
+    subj: str # Noun
+    verb: str # Verb
+    obj:  str # Noun
+
+class Ability(NamedTuple):
+    ability_type: Literal["static", "triggered", "activated"]
+    effect: list[Sentence]
+    cost: list[Union[Mana, Sentence]] = []
+    trigger: Union[Sentence, None] = None
+    keyword: Union[str, None] = None
+
+KeywordLine = list[KeywordAbility]
+Line = Union[KeywordLine, Ability]
+RulesText = list[Line]
+
+class ParsedCard(NamedTuple):
+    mana_cost: Mana
+    typeline: Typeline
+    rules_text: RulesText
---
+++ demystify/visitor.py
@@ -0,0 +1,88 @@
+from typing import Iterable
+
+from inflection import singularize
+
+from grammar.DemystifyVisitor import DemystifyVisitor
+
+from mtg_types import Ability, KeywordAbility, KeywordLine, Line, Mana, ManaSymbol, Typeline
+
+def flatten(container: Iterable):
+    for i in container:
+        if isinstance(i, (list, tuple)):
+            yield from flatten(i)
+        else:
+            yield i
+
+class DemystifyDataVisitor(DemystifyVisitor):
+
+    # Utility methods
+    # Visit everything in a context list
+    def map_visit(self, ctx):
+        if ctx is None:
+            return []
+        else:
+            return list(map(lambda c: self.visit(c), ctx))
+
+    # Terminal
+    def visitTerminal(self, node) -> str:
+        return singularize(node.getText())
+
+
+    # Start rule cardManaCost
+    def visitCardManaCost(self, ctx) -> Mana:
+        return self.visit(ctx.mana())
+
+    def visitMana(self, ctx) -> Mana:
+        return self.map_visit(ctx.manaSymbol())
+
+    def visitManaSymbol(self, ctx) -> ManaSymbol:
+        return ManaSymbol.from_str(self.visitChildren(ctx))
+
+
+    # Start rule typeline
+    def visitTypeline(self, ctx) -> Typeline:
+        supertypes = self.map_visit(ctx.supertype())
+        cardTypes = self.map_visit(ctx.cardType())
+        subtypes = self.map_visit(ctx.subtype())
+        return Typeline(cardTypes, supertypes=supertypes, subtypes=subtypes)
+
+
+    # Start rule rulesText
+    def visitRulesText(self, ctx) -> list[Line]:
+        return self.map_visit(ctx.line())
+
+    def visitKeywordLine(self, ctx) -> KeywordLine:
+        return self.map_visit(ctx.keywordAbility())
+
+    def visitKeywordAbilityIntCost(self, ctx) -> KeywordAbility:
+        return KeywordAbility(
+            self.visit(ctx.kw),
+            arg_int=None if ctx.keywordArgInt() is None else self.visit(ctx.keywordArgInt()),
+            arg_cost=None if ctx.keywordArgCost() is None else self.visit(ctx.keywordArgCost()))
+
+    def visitKeywordAbilityFrom(self, ctx) -> KeywordAbility:
+        return KeywordAbility(
+            self.visit(ctx.keywordFrom()),
+            arg_from=self.map_visit(ctx.keywordArgFrom()))
+
+    def visitKeywordAbilityQuality(self, ctx) -> KeywordAbility:
+        return KeywordAbility(
+            ctx.kw.text,
+            arg_quality=self.visit(ctx.keywordArgQuality()))
+
+    def visitKeywordAbilityQualityCost(self, ctx) -> KeywordAbility:
+        return KeywordAbility(
+            ctx.kw.text,
+            arg_quality=self.visit(ctx.keywordArgQuality()),
+            arg_cost=self.visit(ctx.keywordArgCost()))
+
+    def visitAbility(self, ctx) -> Ability:
+        effect = self.visit(ctx.spellEffect())
+        if ctx.cost() is not None:
+            cost = self.visit(ctx.cost())
+            return Ability("activated", effect, cost=cost)
+        elif ctx.trigger() is not None:
+            trigger = self.visit(ctx.trigger())
+            return Ability("triggered", effect, trigger=trigger)
+        else:
+            return Ability("static", effect)
